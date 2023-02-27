dbc6cc9de2b094da5425a88e099552791dfe5236
Rob Hewison <robhewison1@gmail.com>
2023-02-26 22:44:14 -0500

    cleaned up code

--- assignments/assignment4/form.php
+++ assignments/assignment4/form.php
@@ -7,13 +7,11 @@ if(count($_POST) > 0){
 }
 ?>
 
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Assignment 4</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
@@ -29,8 +27,8 @@ if(count($_POST) > 0){
             <input type="text" class="form-control" name="name">
             <br>
         </form>
+        <label for="namelist" class="form-label">List of Names</label>
         <textarea style="height: 500px;" class="form-control" id="namelist" name="namelist"><?php if(isset($output)) { echo $output; } ?></textarea>
     </div>
-    
 </body>
 </html>
